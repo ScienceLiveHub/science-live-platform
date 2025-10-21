4e34eeed7a809f923ecb158611ca5acb1e490918
Anne Fouilloux <annef@simula.no>
2025-10-21 12:30:33 +0200

    Integrate nanopub-view library
    - Add Font Awesome CDN link for icons
    - Create TestNanopubViewer test page at /test-nanopub route
    - Install @sciencelivehub/nanopub-view as local dependency
    - Remove old internal nanopub viewer components (now using external library)
    - Update .gitignore

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
 import { NanopubTest } from './pages/NanopubTest';
 import { RealNanopubTest } from './pages/RealNanopubTest';
+import { TestNanopubViewer } from './pages/TestNanopubViewer';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/test-parser" element={<NanopubTest />} />
         <Route path="/test-real" element={<RealNanopubTest />} />
+        <Route path="/test-viewer" element={<TestNanopubViewer />} />
       </Routes>
     </Router>
   );
