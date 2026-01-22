1e1ac34dcefc05f5bad182908a4f754f742ea4c1
vijay-prema <vjprema@gmail.com>
2026-01-22 21:51:30 +0000

    chore(*): update license, fixes to devcontainer setup

--- scripts/devcontainer-setup.sh
+++ scripts/devcontainer-setup.sh
@@ -1,9 +1,12 @@
+#!/bin/bash
+
 # Runs on first build of dev container
 
 # Install Zotero PPA
 curl -sL https://raw.githubusercontent.com/retorquere/zotero-deb/master/install.sh | sudo bash
 
 sudo apt update
+sudo apt upgrade -y
 sudo apt install zotero trash-cli -y
 npm install
 npm install -g prpm
