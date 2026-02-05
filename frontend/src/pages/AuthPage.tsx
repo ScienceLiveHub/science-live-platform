c0670ffcbe8e5c8c93d882d86aba1d41939673e2
vijay-prema <vjprema@gmail.com>
2026-02-05 05:33:13 +0000

    fix(auth): improved redirect

--- frontend/src/pages/AuthPage.tsx
+++ frontend/src/pages/AuthPage.tsx
@@ -6,7 +6,7 @@ export default function AuthPage() {
 
   return (
     <main className="container mx-auto flex grow flex-col items-center justify-center gap-3 self-center p-4 md:p-6">
-      <AuthView pathname={pathname} redirectTo="/" />
+      <AuthView pathname={pathname} />
     </main>
   );
 }
